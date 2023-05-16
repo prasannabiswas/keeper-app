8d11cdb4320f0cef59e7df1dace66320cb41a0b4
Prasanna Biswas <probiswas82@gmail.com>
2023-05-17 04:13:53 +0530

    index import unused variable delete

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './component/App';
-import Header from './component/Header';
-import Footer from './component/Footer';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
